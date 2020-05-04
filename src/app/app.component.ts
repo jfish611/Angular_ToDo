e2c100dc6d198d85f92a709a44e751788f91caf2
Jacob Fish <jacobfish@quickenloans.com>
2020-05-04 09:00:00 -0400

    committing project code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ToDo} from './Interfaces/ToDos';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularToDo';
+ 
+  todoTask:string;
+
+  title = 'Angular App #1 To-Do';
+  ToDos: ToDo[] = [
+    {
+      task: "Take Rafi to the park",
+      completed: true,
+      editing: false,
+    },
+    {
+      task: "Daily Daf Yomi",
+      completed: true,
+      editing: false,
+    },
+    {
+      task: "Work on Angular stuff",
+      completed: false,
+      editing: false,
+    }
+    
+  ]
+
+  // displayCongratulations(){
+
+  // }
+
+  // searchTasks(input:string){
+  //   let filteredTasks = this.ToDos.filter(fa){
+  //     task.contains(input)
+  //   }
+  // }
+
+  addToDo(){
+    if(this.todoTask.length===0){
+      return;
+    }
+   this.ToDos.push({
+      task: this.todoTask,
+      completed: false,
+      editing: false,
+    })
+    this.todoTask='';
+  }
+
+completeToDo(a:ToDo){ 
+a.completed=true;
 }
+
+editToDo(a:ToDo):void{
+  a.editing=!a.editing
+}
+ 
+deleteToDo(a:number):void{
+  console.log('got to deleteToDo:',a);
+  this.ToDos.splice(a,1)
+
+}
+}
+
+
+
